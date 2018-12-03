a53855932787b6d8dd749463cee2441b1f6b774f
Ubuntu <ubuntu@ip-172-31-17-183.us-west-2.compute.internal>
2018-12-03 22:47:30 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,7 +10,55 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @guesses_count = 0
   end
+
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  def guess(char)
+    @guesses_count += 1
+    unless char =~ /^[a-z]$/i
+      raise ArgumentError
+    end
+    char.downcase!
+    unless (@guesses + @wrong_guesses).include? char
+      if @word.include? char
+        # byebug
+        @guesses= @guesses + char
+      else
+        @wrong_guesses= @wrong_guesses + char
+      end
+      true
+    else
+      false
+    end
+  end
+
+  def word_with_guesses
+    displayed_guesses = ''
+    @word.each_char do |c|
+      if guesses.include? c
+        displayed_guesses += c
+      else
+        displayed_guesses += '-'
+      end
+    end
+    displayed_guesses
+  end
+
+  def check_win_or_lose
+    if @guesses_count >= 7
+      :lose
+    elsif word_with_guesses.include? '-'
+      :play
+    else
+      :win
+    end
+  end
+        
+      
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
@@ -23,5 +71,4 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
-
 end
